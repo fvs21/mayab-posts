53b3275ec99193bed358240e818a5eb4d9f5a7ee
emiliorevv <emilio.revueltas@gmail.com>
2025-09-28 15:02:17 -0600

    Agregue el JWT Token a la función de actualizar la bio

--- backend/app/user/routes.py
+++ backend/app/user/routes.py
@@ -7,13 +7,14 @@
 '''
 from flask import Blueprint, jsonify, request
 from app.user.service import update_user_bio
-
+from flask_jwt_extended import jwt_required, get_jwt_identity
 user_bp = Blueprint("user", __name__, url_prefix="/user") #Se crea el blueprint para poder usar con el prefix de user
 
 @user_bp.route('/bio', methods=['PUT']) #Crea una ruta en el blueprint de usuario la cual se encarga de actualizar la bio
+@jwt_required() #Se encarga de proteger la ruta para que solo los usuarios indicados puedan acceder a ella
 def update_bio(): #Funcion que se activara cuando alguien haga peticion PUT a /user/bio
     data = request.get_json() #Obtenemos la información que nos acaba de llegar, la interpretamos en formato JSON y la guardamos en data
-    user_id = data.get('user_id')  #Intenta obtener el valor asociado a 'user_id' y guardarlo como una variable, si no existe devuelve None
+    user_id = get_jwt_identity() #Obtenemos el id del usuario a traves del JWT Token
     new_bio = data.get('bio') #Intenta obtener el valor asociado a 'bio' y guardarlo como una variable, si no existe devuelve None
 
     if not user_id or not new_bio: #Se encarga de verificar si alguna de las variables es None o esta vacia
